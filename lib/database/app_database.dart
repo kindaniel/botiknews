9097aa7c77a76d9b87165ec977d3501d1162f0da
Daniel Kin Ivatiuk <daniel.ivatiuk@gmail.com>
2021-03-23 18:48:14 -0300

    fix: some inserts

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -13,8 +13,8 @@ Future<Database> getDatabase() async {
     db.rawInsert(
         'INSERT INTO users(id, name, email, password) VALUES(100,"Alice", "bob@info.com", "123123")');
     db.rawInsert(
-        'INSERT INTO posts(id, date, content_text, user_id) VALUES(99,"2020-11-02T16:10:33Z", "Pessoal!! Tem um candidato aí que é excelente!! Prestem atenção. Com ele, podemos entregar coisas ainda melhores!", 99)');
+        'INSERT INTO posts(id, date, content_text, user_id) VALUES(99,"2021-03-23T16:10:33Z", "Pessoal!! Tem um candidato aí que é excelente!! Prestem atenção. Com ele, podemos entregar coisas ainda melhores!", 99)');
     db.rawInsert(
-        'INSERT INTO posts(id, date, content_text, user_id) VALUES(100,"2020-11-03T16:10:33Z", "Concordo Bob. Sei de quem você está falando, é sobre o Daniel Kin! É realmente um ótimo profissional. Vai somar bastante no time de mobile. ", 100)');
+        'INSERT INTO posts(id, date, content_text, user_id) VALUES(100,"2021-11-23T16:20:33Z", "Concordo Bob. Sei de quem você está falando, é sobre o Daniel Kin! É realmente um ótimo profissional. Vai somar bastante no time de mobile. ", 100)');
   }, version: 1, onDowngrade: onDatabaseDowngradeDelete);
 }
